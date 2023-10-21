76766f0df80cdcf6ba087e7dae8ab89d6ad0980d
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2023-10-21 20:34:10 +0100

    json loader

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,16 +1,30 @@
-async function fetchAndParseXML() {
-  let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );
-  let text = await prom.text();
-  
-  console.log( text.substring(0, 100 ) );
-  
-  let parser = new DOMParser();
-  let xmlDoc = parser.parseFromString(text, "text/xml");
-  
-  console.log( xmlDoc );
-  
-  const items = xmlDoc.querySelectorAll("item");
-  console.log('items:', items );
-}
+var $textarea = $('<textarea>').attr('id', 'ta0').appendTo('body');
+var $loadbutton = $('<button>Load</button>').appendTo('body');
+var $savebutton = $('<button>Save</button>').appendTo('body');
 
-fetchAndParseXML();
+$textarea.val('Enter text');
+
+$savebutton.click(function() {
+  var textContent = $textarea.val();
+  var blob = new Blob([textContent], { type: 'octet/stream' });
+  var url = window.URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = 'afile.txt';
+  a.click();
+  window.URL.revokeObjectURL(url);
+});
+
+$loadbutton.click(function() {
+  var input = $('<input type="file">');
+  input.on('change', function(e) {
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      reader.onload = function(event) {
+          var fileContents = event.target.result;
+          $textarea.val(fileContents);
+      };
+      reader.readAsText(file);
+  });
+  input.click();
+})
