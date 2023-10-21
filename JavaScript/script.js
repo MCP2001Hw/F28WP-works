6967a02718eeda5db93dee83b488f308c8583e46
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2023-10-21 09:31:29 +0100

    Half-ly working...?

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,14 @@ async function fetchAndParseXML() {
   let text = await prom.text();
   
   console.log( text.substring(0, 100 ) );
+  
+  let parser = new DOMParser();
+  let xmlDoc = parser.parseFromString(text, "text/xml");
+  
+  console.log( xmlDoc );
+  
+  const items = xmlDoc.querySelectorAll("item");
+  console.log('items:', items );
 }
 
 fetchAndParseXML();
