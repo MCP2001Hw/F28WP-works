c1c1df1a37572d23f791fb86a926ecd904f45cb5
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2023-11-02 08:37:10 +0000

    Local storage test

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,30 +1,17 @@
-var $textarea = $('<textarea>').attr('id', 'ta0').appendTo('body');
-var $loadbutton = $('<button>Load</button>').appendTo('body');
-var $savebutton = $('<button>Save</button>').appendTo('body');
+function saveData() {
+  const dataInput = document.getElementById('data-input');
+  localStorage.setItem('myData', dataInput.value);
+  displayData();
+}
 
-$textarea.val('Enter text');
+function clearData() {
+  localStorage.removeItem('myData');
+  displayData();
+}
 
-$savebutton.click(function() {
-  var textContent = $textarea.val();
-  var blob = new Blob([textContent], { type: 'octet/stream' });
-  var url = window.URL.createObjectURL(blob);
-  var a = document.createElement('a');
-  a.href = url;
-  a.download = 'afile.txt';
-  a.click();
-  window.URL.revokeObjectURL(url);
-});
+function displayData() {
+  const dataOutput = document.getElementById('data-output');
+  dataOutput.textContent = `Local Storage Data: ${localStorage.getItem('myData') || 'No Data'}`;
+}
 
-$loadbutton.click(function() {
-  var input = $('<input type="file">');
-  input.on('change', function(e) {
-      var file = e.target.files[0];
-      var reader = new FileReader();
-      reader.onload = function(event) {
-          var fileContents = event.target.result;
-          $textarea.val(fileContents);
-      };
-      reader.readAsText(file);
-  });
-  input.click();
-})
+displayData();
