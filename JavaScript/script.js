40a555d5e918bb1aea6cb98a930b6799c9c525f5
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2023-11-15 19:17:32 +0000

    RNG with Node.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,22 @@
-function saveData() {
-  const dataInput = document.getElementById('data-input');
-  sessionStorage.setItem('myData', dataInput.value);
-  displayData();
+const fs = require('fs');
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
 }
 
-function clearData() {
-  sessionStorage.removeItem('myData');
-  displayData();
+function checkEvenOrOdd(number) {
+  return number % 2 === 0 ? 'even' : 'odd';
 }
 
-function displayData() {
-  const dataOutput = document.getElementById('data-output');
-  dataOutput.textContent = `Session Storage Data: ${sessionStorage.getItem('myData') || 'No Data'}`;
-}
+const randomNumber = generateRandomNumber();
+const result = checkEvenOrOdd(randomNumber);
 
-displayData();
+console.log(`Random number: ${randomNumber}\nThe number is ${result}.`);
+
+fs.writeFile('result.txt', `Random number: ${randomNumber}\nThe number is ${result}.`, (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+  } else {
+    console.log('Result written to result.txt');
+  }
+});
